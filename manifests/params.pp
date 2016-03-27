09cc7c018aff859c84c83c2566fc8a8fc61ae009
Steve ESSO <esso_s@dladc2-infpup01.mtncameroon.net>
2016-03-27 18:39:52 +0100

    Included Management of maximum and minimum password age for Local User if needed

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,17 @@
 class password_aging::params {
 
+
+  $users_to_manage          = []
+  $manage_existing_user     = false
+
+
   case $::kernel {
     'linux' : {
-      $pass_max_days      = "99999"
-      $pass_min_days      = "0"
-      $pass_min_len       = "5" 
-      $pass_warn_age      = "7"
-      $params_file        = "/etc/login.defs"
+      $pass_max_days        = "99999"
+      $pass_min_days        = "0"
+      $pass_min_len         = "5" 
+      $pass_warn_age        = "7"
+      $params_file          = "/etc/login.defs"
     }
     # if we are not on a supported os, do not do anything.
     default: { }
